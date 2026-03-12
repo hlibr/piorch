807a5afe237a4d97b3b8fa663b0e58d7f02408c5
Gleb <rakka.chama@gmail.com>
2026-03-12 19:43:33 +0200

    Add CI workflow and tooling config

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json"
+      }
+    }
+  }
+];
